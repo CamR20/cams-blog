ef4b145ef9cd7dba587ca2cd46a80dcbc7ac7555
Cameron Rouse <cam-rouse@hotmail.co.uk>
2020-09-13 19:03:50 +0100

    sunday night create articles started

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    # to create you need top level article, and the only permitted attributes are title and description
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # save back to the database
+    @article.save
+    # now redirect to articles show page, so you can see its been added. check with rails c that it is in database. Article.all
+    # check routes. it says article/:id, so it would be as below
+    redirect_to (@article)
+  end
 end
--- app/models/Article.rb
+++ app/models/Article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 20, maximum: 300 }
-
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+# articles wont save if they dont meet the validations above
 end
