b256252f112ba6d4b75472712724c8dc40566cd0
Cameron Rouse <cam-rouse@hotmail.co.uk>
2020-09-14 09:18:04 +0100

    edit and update complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    # same line as show, as needs to find specific article to be edited. nothing will happen until update is set up
+    @article = Article.find(params[:id])
+  end
+
   def create
     # to create you need top level article, and the only permitted attributes are title and description
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -24,6 +29,19 @@ class ArticlesController < ApplicationController
     else
       # if it doesnt work, we need to re render the form to fill out again
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    # will only update if the below requirements are met
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      # redirects to article show page
+      redirect_to @article
+    else
+      # if not filled in properly, the edit form will be rendered again so that user has to resubmit
+      render 'edit'
     end
   end
 end
