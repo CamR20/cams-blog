36267eb92dda74aa0f6669ec501396ee77ac752e
Cameron Rouse <cam-rouse@hotmail.co.uk>
2020-09-14 08:43:38 +0100

    monday morning form validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def show
     @article = Article.find(params[:id])
   end
@@ -9,15 +8,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     # to create you need top level article, and the only permitted attributes are title and description
     @article = Article.new(params.require(:article).permit(:title, :description))
     # save back to the database
-    @article.save
+    if @article.save
+      # shows user that the article has been saved. see application.html for each method
+      flash[:notice] = "Article was created sucessfully!"
     # now redirect to articles show page, so you can see its been added. check with rails c that it is in database. Article.all
     # check routes. it says article/:id, so it would be as below
-    redirect_to (@article)
+      redirect_to (@article)
+    else
+      # if it doesnt work, we need to re render the form to fill out again
+      render 'new'
+    end
   end
 end
